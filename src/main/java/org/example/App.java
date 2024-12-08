45a911b3200a7cea39c0afe6aa232391db32a832
AVFaletyonok <AVFaletyonok@yandex.ru>
2024-12-08 23:40:02 +0300

    Revert "homeworks/homework01 Copied 02 homework"

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,13 @@
 package org.example;
 
-import java.util.ArrayList;
-
 /**
  * Hello world!
  *
  */
-public class App
+public class App 
 {
     public static void main( String[] args )
     {
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product(1L, "milk", 100L, 2L));
-        products.add(new Product(2L, "bred", 50L, 3L));
-        products.add(new Product(3L, "meat", 200L, 2L));
-
-        System.out.println(products.toString());
+        System.out.println( "Hello World!" );
     }
 }
